5309509b5bef0b31536763dc3a0fbb76fa42f7c7
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-05 13:08:03 -0400

    Update Server.lua

--- module/Server/Server.lua
+++ module/Server/Server.lua
@@ -1,6 +1,7 @@
 server = {}
 remote = require(script.Parent.Core.Remote)
 core = require(script.Parent.Core.Core)
+admin = require(script.Parent.Admin.Admin)
 
 function server:Initialize()
     warn('Superduperdev2 Admin Commands: Loading..')
