a3bae29210980876806f601a9c194b48ace57cc2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-05 13:04:07 -0400

    Create Admin.lua

---
+++ module/Server/Admin/Admin.lua
@@ -0,0 +1,13 @@
+admin = {}
+server = require(script.Parent.Parent.Server)
+client = require(script.Parent.Parent.Parent.Client.Client)
+remote = require(script.Parent.Parent.Core.Remote)
+core = require(script.Parent.Parent.Core.Core)
+
+function admin:RunCommand(msg,args)
+    if msg~=nil then
+        
+    end
+end
+
+return admin
