a9219279edd5e34370ebc26b47dd0e14dbea84d5
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-06 07:51:27 -0400

    Create Client.lua

---
+++ module/Client/Client.lua
@@ -0,0 +1,13 @@
+client = {
+    Prefix = ';',
+}
+server = require(script.Parent.Parent.Server.Server)
+remote = server.Remote
+
+function client:Initialize(plr)
+    if plr~=nil then
+        
+    end
+end
+
+return client
